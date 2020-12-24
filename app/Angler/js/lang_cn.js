4b86af9dd1d8c9c1221cfa71db99261a594b12a7
anlei <254810452@qq.com>
2020-12-25 03:30:47 +0800

    20201225-1
    20201225-1

---
+++ app/Angler/js/lang_cn.js
@@ -0,0 +1,24 @@
+var lang_var = {
+	tab_menu:{
+		lab:{
+			mission:"任务",
+			me:"我的"
+		},
+		me:{
+			lab:{
+				user_email:"用户邮箱:",
+				commission:"总佣金",
+				principal:"总本金",
+				public_notice:"公告",
+				capital_details:"资金明细",
+				binding_paypal:"绑定Paypal",
+				draw_money:"提现",
+				share_make_money:"分享赚钱",
+				help:"帮助",
+				my_business_partner:"我的合伙人",
+				logout:"退出登录",
+			}
+		}
+	},
+	
+};
